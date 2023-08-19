1cc307fd2ea80a07159114a29f862756d3732901
Awais <awaisdev2@gmail.com>
2023-08-19 13:58:42 +0500

    create server in backend

--- Chat-Backend/index.js
+++ Chat-Backend/index.js
@@ -1,9 +1,27 @@
 const express = require("express")
 const app = express()
 const http = require("http")
+const cors = require("cors")
+const { Server } = require("socket.io")
+
+app.use(cors())
 
 const server = http.createServer(app)
 
-server.listen(9000, ()=>{
+const io = new Server(server, { 
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+})
+
+io.on("connection", (socket) =>{ 
+    console.log(`User connected: ${socket.id}`)
+    io.on("disconnect", () =>{
+        console.log(console.log("User Disconnected"), socket.id)
+    })
+})
+ 
+server.listen(4500, ()=>{
     console.log("SERVER IS RUNNING SUCCESSFULLY!")
 })
